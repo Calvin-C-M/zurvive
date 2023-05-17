1906ad62b2fec2671156d88ca108214a3f8f832b
Calvin-C-M <montolalu28@gmail.com>
2023-05-18 01:02:16 +0700

    Zombie can now spawn with intervals

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Spawner : MonoBehaviour
 {
     public int totalZombies;
 
     public GameObject zombie;
+    public float spawnInterval; // In minutes
+    public int currentWave;
+    private int initWave;
+    private TMP_Text waveText;
+    private bool canSpawn;
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.totalZombies = 3;
+        this.spawnInterval = 2f;
+        this.initWave = 0;
+        this.currentWave = this.initWave;
+        this.waveText = GetComponentInChildren<TMP_Text>();
+        this.canSpawn = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F2))
+        if(this.canSpawn)
         {
-            this.SpawnZombie();
+            StartCoroutine(this.ChangeWave());
+        }
+        else
+        {
+            Debug.Log("Zombie cannot spawn yet");
         }
     }
 
@@ -31,5 +47,21 @@ public class Spawner : MonoBehaviour
             spawnPos.x += i;
             Instantiate(zombie, spawnPos, Quaternion.identity);
         }
+    }
+
+    private void SetWave()
+    {
+        this.currentWave++;
+        this.waveText.text = "Wave \n" + this.currentWave;
+        this.SpawnZombie();
+    }
+
+    private IEnumerator ChangeWave()
+    {
+        this.SetWave();
+        float second = 6f;
+        this.canSpawn = false;
+        yield return new WaitForSeconds(this.spawnInterval * second);
+        this.canSpawn = true;
     }
 }
