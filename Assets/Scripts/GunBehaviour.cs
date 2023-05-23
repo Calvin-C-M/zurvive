8514ab5036cf4b48a70fddebbcd658e5dfecee53
Calvin-C-M <montolalu28@gmail.com>
2023-05-24 03:06:31 +0700

    Divide weapon behaviour script

---
+++ Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GunBehaviour : WeaponBehaviour
+{
+    protected bool isReloading;
+    protected bool allowFire;
+    protected TMP_Text ammoText;
+    protected ParticleSystem[] muzzleParticles;
+    protected GameObject muzzleLight;
+    protected AudioSource firingSound;
+    protected AudioSource reloadSound;
+    
+    // Start is called before the first frame update
+    void Start() 
+    {
+        base.Init();
+        this.isReloading = false;
+        this.allowFire = true;
+
+        // Control muzzle particle attributes
+        this.muzzleParticles = GetComponentsInChildren<ParticleSystem>();
+        foreach(ParticleSystem particle in this.muzzleParticles)
+        {
+            particle.Stop();
+        }        
+        this.muzzleLight = GameObject.Find("Light");
+        this.muzzleLight.SetActive(false);
+
+        // Control weapon sound components
+        AudioSource[] sounds = GetComponentsInChildren<AudioSource>();
+        foreach(AudioSource sound in sounds) 
+        {
+            sound.volume = 0.5f;
+        }
+        this.firingSound = sounds[0];
+        this.reloadSound = sounds[1];
+
+        this.ammoText = GetComponentInChildren<TMP_Text>();
+        this.ChangeAmmoText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.Controls();
+    }
+    
+    private void Controls()
+    {
+        if(Input.GetMouseButton(0) && this.allowFire && this.ammo > 0 && !this.isReloading)
+        {
+            StartCoroutine(this.Shoot());
+        }
+        if(Input.GetKeyDown("r"))
+        {
+            this.Reload();
+        }
+    }
+
+    private void Fire()
+    {
+        this.ammo -= 1;
+        this.ChangeAmmoText();
+        this.firingSound.Play();
+        StartCoroutine(this.ToggleMuzzleFlash());
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, this.range))
+        {
+            GameObject zombie = hit.collider.gameObject;
+            ZombieBehaviour zombieAttr = zombie.GetComponent<ZombieBehaviour>();
+            CharacterBehaviour characterAttr = GetComponentInParent<CharacterBehaviour>();
+            if(zombieAttr != null)
+            {
+                zombieAttr.TakeDamage(this.damage);
+                if(zombieAttr.health <= 0)
+                {
+                    Destroy(zombie);
+                    characterAttr.killCounter++;
+                }
+            }
+        }
+    }
+
+    private void Reload()
+    {
+        StartCoroutine(this.PlayReload());
+        this.ammo = this.initAmmo;
+        this.ChangeAmmoText();
+    }
+
+    private IEnumerator PlayReload()
+    {
+        this.isReloading = true;
+        this.reloadSound.Play();
+        yield return new WaitForSeconds(1.0f);
+        this.isReloading = false;
+        this.reloadSound.Stop();
+    }
+
+    private IEnumerator Shoot()
+    {
+        this.allowFire = false;
+        this.Fire();
+        yield return new WaitForSeconds(this.fireRate);
+        this.allowFire = true;
+    }
+
+    private IEnumerator ToggleMuzzleFlash()
+    {
+        foreach(ParticleSystem particle in this.muzzleParticles)
+        {
+            particle.Play();
+        }
+        this.muzzleLight.SetActive(true);
+
+        yield return new WaitForSeconds(0.3f);
+
+        foreach(ParticleSystem particle in this.muzzleParticles)
+        {
+            particle.Stop();
+        }
+        this.muzzleLight.SetActive(false);
+    }
+    
+    private void ChangeAmmoText()
+    {
+        this.ammoText.text = this.ammo + " / " + this.initAmmo;
+    }
+}
--- Assets/Scripts/WeaponBehaviour.cs
+++ Assets/Scripts/WeaponBehaviour.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 [SerializeField]
 public class WeaponBehaviour : MonoBehaviour
@@ -11,39 +10,13 @@ public class WeaponBehaviour : MonoBehaviour
     public float range;
     public float fireRate;
     public float ammo;
-    private float initAmmo;
-    private bool allowFire = true;
-    private bool isReloading = false;
-    private ParticleSystem[] muzzleParticles;
-    private GameObject muzzleLight;
-    private AudioSource firingSound;
-    private AudioSource reloadSound;
-    private TMP_Text ammoText;
-    private WeaponDict weaponDictionary;
+    protected float initAmmo;
+    protected WeaponDict weaponDictionary;
 
 
     // Start is called before the first frame update
-    void Start()
+    protected void Init()
     {
-        // Control weapon particle components
-        this.muzzleParticles = GetComponentsInChildren<ParticleSystem>();
-        foreach(ParticleSystem particle in this.muzzleParticles)
-        {
-            particle.Stop();
-        }        
-
-        this.muzzleLight = GameObject.Find("Light");
-        this.muzzleLight.SetActive(false);
-
-        // Control weapon sound components
-        AudioSource[] sounds = GetComponentsInChildren<AudioSource>();
-        foreach(AudioSource sound in sounds) 
-        {
-            sound.volume = 0.5f;
-        }
-        this.firingSound = sounds[0];
-        this.reloadSound = sounds[1];
-
         // Control weapon attributes
         this.weaponType = (this.weaponType == "") ? "rifle" : this.weaponType.ToLower(); 
         this.weaponDictionary = new WeaponDict();
@@ -52,99 +25,11 @@ public class WeaponBehaviour : MonoBehaviour
         this.fireRate = this.weaponDictionary.attribute[weaponType]["fireRate"];
         this.initAmmo = this.weaponDictionary.attribute[weaponType]["initAmmo"];
         this.ammo = this.initAmmo;
-        
-        this.ammoText = GetComponentInChildren<TMP_Text>();
-        this.ChangeAmmoText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.Controls();
-    }
 
-    private void Controls()
-    {
-        if(Input.GetMouseButton(0) && this.allowFire && this.ammo > 0 && !this.isReloading)
-        {
-            StartCoroutine(this.Shoot());
-        }
-        if(Input.GetKeyDown("r"))
-        {
-            this.Reload();
-        }
-    }
-
-    private void Fire()
-    {
-        this.ammo -= 1;
-        this.ChangeAmmoText();
-        this.firingSound.Play();
-        StartCoroutine(this.ToggleMuzzleFlash());
-
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if(Physics.Raycast(ray, out hit, this.range))
-        {
-            GameObject zombie = hit.collider.gameObject;
-            ZombieBehaviour zombieAttr = zombie.GetComponent<ZombieBehaviour>();
-            CharacterBehaviour characterAttr = GetComponentInParent<CharacterBehaviour>();
-            if(zombieAttr != null)
-            {
-                zombieAttr.TakeDamage(this.damage);
-                if(zombieAttr.health <= 0)
-                {
-                    Destroy(zombie);
-                    characterAttr.killCounter++;
-                }
-            }
-        }
-    }
-
-    private void Reload()
-    {
-        StartCoroutine(this.PlayReload());
-        this.ammo = this.initAmmo;
-        this.ChangeAmmoText();
-    }
-
-    private IEnumerator Shoot()
-    {
-        this.allowFire = false;
-        this.Fire();
-        yield return new WaitForSeconds(this.fireRate);
-        this.allowFire = true;
-    }
-
-    private IEnumerator ToggleMuzzleFlash()
-    {
-        foreach(ParticleSystem particle in this.muzzleParticles)
-        {
-            particle.Play();
-        }
-        this.muzzleLight.SetActive(true);
-
-        yield return new WaitForSeconds(0.3f);
-
-        foreach(ParticleSystem particle in this.muzzleParticles)
-        {
-            particle.Stop();
-        }
-        this.muzzleLight.SetActive(false);
-    }
-
-    private IEnumerator PlayReload()
-    {
-        this.isReloading = true;
-        this.reloadSound.Play();
-        yield return new WaitForSeconds(1.0f);
-        this.isReloading = false;
-        this.reloadSound.Stop();
-    }
-
-    private void ChangeAmmoText()
-    {
-        this.ammoText.text = this.ammo + " / " + this.initAmmo;
     }
 }
